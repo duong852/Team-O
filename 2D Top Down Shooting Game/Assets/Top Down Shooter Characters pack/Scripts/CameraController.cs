f2f5d152451bad82afe30fb5cfe3270fb06185c8
Duong Nguyen <59811507+duong852@users.noreply.github.com>
2020-02-28 01:12:38 -0500

    fix camera issue and finalized the map

--- 2D Top Down Shooting Game/Assets/Top Down Shooter Characters pack/Scripts/CameraController.cs
+++ 2D Top Down Shooting Game/Assets/Top Down Shooter Characters pack/Scripts/CameraController.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    GameObject player;
-    Vector3 offset;
+	public Transform Player;
+	private Vector3 offset;
+	void Start()
+	{
+		offset = transform.position - Player.transform.position;
+	}
+	void LateUpdate()
+	{
+		transform.position = Player.transform.position + offset;
+	}
 
-    void Start()
-    {
-    
-        offset = transform.position - player.transform.position;
-    }
-    void Update()
-    {
-
-    }
-    void LateUpdate()
-    {
-        float newXPosition = player.transform.position.x - offset.x;
-        float newZPosition = player.transform.position.z - offset.z;
-
-        transform.position = new Vector3(newXPosition, transform.position.y, newXPosition);
-    }
 }
