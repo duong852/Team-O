eb6a8dd350bc9d60f5ac8330270a69ba7512e86e
Duong Nguyen <59811507+duong852@users.noreply.github.com>
2020-04-05 01:14:56 -0400

    save the process before doing any risky things

--- 2D Top Down Shooting Game/Assets/Builds/Scripts/BulletController.cs
+++ 2D Top Down Shooting Game/Assets/Builds/Scripts/BulletController.cs
@@ -1,55 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
-public class bulletController : MonoBehaviour {
-public string RedTeamTag, BlueTeamTag;
-public Transform parentTransform;
-public string parentTag; 
-public float bulletSpeed = 100f;
-public int Damage = 1;
-[HideInInspector]
-public float Wait = 0;
-public Sprite hitSprite;	
+public class bulletController : MonoBehaviour
+{
+	public string RedTeamTag, BlueTeamTag;
+	public Transform parentTransform;
+	public string parentTag;
+	public float bulletSpeed = 100f;
+	public int Damage = 1;
+	[HideInInspector]
+	public float Wait = 0;
+	public Sprite hitSprite;
 	// Use this for initialization
-	void Start ()
-		{		
-			GetComponent<Rigidbody2D>().AddRelativeForce( Vector2.right * bulletSpeed);
-			Destroy (gameObject, 2f);						
-		}
-		void FixedUpdate ()
-		{
-			if (Wait < 0.6f)
-			Wait += 0.1f; 
-		}	
-		void OnTriggerEnter2D(Collider2D trig)
-		{
-
-            if (trig.gameObject.tag == "Wall" || trig.gameObject.tag == "Door")
-            {
-                GetComponent<SpriteRenderer>().sprite = hitSprite;
-                Destroy (gameObject, 0.02f);
-            }
-            if (trig.gameObject.tag == "Untagged")
-			{
-				GetComponent<SpriteRenderer> ().sprite = hitSprite;
-				Destroy (gameObject, 0.02f);
-			}
-            if (trig.gameObject.tag == RedTeamTag)
-			{
-				GetComponent<SpriteRenderer> ().sprite = hitSprite;
-				trig.gameObject.GetComponentInChildren<Enemy_Control>().Damage = Damage;
-                if (trig.gameObject != null && trig.gameObject.GetComponent<Folow_Point_Control>().targetInSight == false && parentTag == BlueTeamTag)
-                {
-                    trig.gameObject.GetComponent<Folow_Point_Control>().targetInSight = true;
-                    trig.gameObject.GetComponent<Folow_Point_Control>().AimTarget = parentTransform;
-                }
-                Destroy (gameObject, 0.02f);
-			}
-            if (trig.gameObject.tag == BlueTeamTag)
-			{                
-                GetComponent<SpriteRenderer> ().sprite = hitSprite;
-				trig.gameObject.GetComponentInChildren<PlayerController>().Damage = Damage;                
-				Destroy (gameObject, 0.02f);
-			}			
-        }
+	void Start()
+	{
+		GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * bulletSpeed);
+		Destroy(gameObject, 2f);
 	}
+	void FixedUpdate()
+	{
+		if (Wait < 0.6f)
+			Wait += 0.1f;
+	}
+	void OnTriggerEnter2D(Collider2D trig)
+	{
+
+		if (trig.gameObject.tag == "Wall" || trig.gameObject.tag == "Door")
+		{
+			GetComponent<SpriteRenderer>().sprite = hitSprite;
+			Destroy(gameObject, 0.02f);
+		}
+		if (trig.gameObject.tag == "Untagged")
+		{
+			GetComponent<SpriteRenderer>().sprite = hitSprite;
+			Destroy(gameObject, 0.02f);
+		}
+		if (trig.gameObject.tag == RedTeamTag)
+		{
+			GetComponent<SpriteRenderer>().sprite = hitSprite;
+			trig.gameObject.GetComponentInChildren<Enemy_Control>().Damage = Damage;
+			if (trig.gameObject != null && trig.gameObject.GetComponent<Folow_Point_Control>().targetInSight == false && parentTag == BlueTeamTag)
+			{
+				trig.gameObject.GetComponent<Folow_Point_Control>().targetInSight = true;
+				trig.gameObject.GetComponent<Folow_Point_Control>().AimTarget = parentTransform;
+			}
+			Destroy(gameObject, 0.02f);
+		}
+		if (trig.gameObject.tag == BlueTeamTag)
+		{
+			GetComponent<SpriteRenderer>().sprite = hitSprite;
+			trig.gameObject.GetComponentInChildren<PlayerController>().Damage = Damage;
+			Destroy(gameObject, 0.02f);
+		}
+	}
+}
 
