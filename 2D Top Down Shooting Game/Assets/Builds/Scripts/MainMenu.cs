ec272cee0bb7db394c56cf331bdac9d402827305
Duong Nguyen <59811507+duong852@users.noreply.github.com>
2020-04-06 17:38:53 -0400

    Revert "finished main menu"
    This reverts commit 3b4f6751ee60d924c05e2ccfecbe5d0be4643ed7.

--- 2D Top Down Shooting Game/Assets/Builds/Scripts/MainMenu.cs
+++ 2D Top Down Shooting Game/Assets/Builds/Scripts/MainMenu.cs
@@ -2,30 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public string firstLevel;
-    //public string secondLevel;
-    public GameObject optionsScreen;
-    public void StartGame()
+    public void PlayGame()
     {
-        SceneManager.LoadScene(firstLevel);
-    }
-
-    public void OpenOptions()
-    {
-        optionsScreen.SetActive(true);
-    }
-
-    public void CloseOptions()
-    {
-        optionsScreen.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
     public void QuitGame()
     {
+        Debug.Log("QUIT!");
         Application.Quit();
     }
 }
