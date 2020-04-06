5e54a5a6b402d597f528d156f0cb77f1c1d07cf6
FilaAndrew <fila791@gmail.com>
2020-04-05 21:20:28 -0400

    updated start and end menu, made in level scene managemnet

---
+++ 2D Top Down Shooting Game/Assets/OpitionsMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpitionsMenu : MonoBehaviour
+{
+    public void setVolume(float volume)
+    {
+        
+    }
+}
